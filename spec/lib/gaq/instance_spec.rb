ee0e2d4b74218ccaed94981cb6ae70156f033bb7
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-01-13 17:34:28 +0100

    spec in addition behaviour with non-empty flash (including what is probably a bug, only for now)

--- spec/lib/gaq/instance_spec.rb
+++ spec/lib/gaq/instance_spec.rb
@@ -117,6 +117,115 @@ module Gaq
               be_eql([["_setAccount", nil], ["_setCustomVar", 0, :var, "blah", 3]])
           end
         end
+
+        context "with both next_request.track_event and setting variable on next_request result" do
+          before(:each) do
+            subject.next_request.track_event 'category', 'action', 'label'
+            subject.next_request.var = 'blah'
+          end
+
+          it 'has data on the flash we are interested in' do
+            expected_flash = {:analytics_instructions=>[[["_setCustomVar", 0, :var, "blah", 3]], [["_trackEvent", "category", "action", "label"]]]}
+            flash.should be_eql(expected_flash)
+          end
+        end
+      end
+    end
+
+    context "with a non-empty flash" do
+      let(:flash_from_last_request) do
+        {:analytics_instructions=>[[["_setCustomVar", 0, :var, "blah", 3]], [["_trackEvent", "last_cat", "last_action", "last_label"]]]}
+      end
+
+      let(:flash) { flash_from_last_request }
+
+      def be_gaq_instructions_from_previous_request
+        be_eql [["_setAccount", nil], ["_setCustomVar", 0, :var, "blah", 3], ["_trackEvent", "last_cat", "last_action", "last_label"]]
+      end
+
+      it 'renders properly' do
+        subject.gaq_instructions.should be_gaq_instructions_from_previous_request
+        flash.should be_equal(flash_from_last_request)
+      end
+
+      context "with track_event" do
+        before(:each) do
+          subject.track_event 'category', 'action', 'label'
+        end
+
+        it 'renders properly' do
+          flash.should be_equal(flash_from_last_request)
+
+          subject.gaq_instructions.should \
+            be_eql([["_setAccount", nil], ["_setCustomVar", 0, :var, "blah", 3], ["_trackEvent", "last_cat", "last_action", "last_label"], ["_trackEvent", "category", "action", "label"]])
+        end
+      end
+
+      context "with next_request.track_event" do
+        before(:each) do
+          subject.next_request.track_event 'category', 'action', 'label'
+        end
+
+        it 'renders properly' do
+          flash.should be_eql({:analytics_instructions=>[[], [["_trackEvent", "category", "action", "label"]]]})
+
+          subject.gaq_instructions.should be_gaq_instructions_from_previous_request
+        end
+      end
+
+      context "with a variable declared" do
+        before(:each) do
+          variables << { name: :var, scope: 3, slot: 0 }
+        end
+
+        context "...but not assigned" do
+          it 'renders properly' do
+            flash.should be_equal(flash_from_last_request)
+
+            subject.gaq_instructions.should be_gaq_instructions_from_previous_request
+          end
+        end
+
+        context "after assigning to variable" do
+          before(:each) do
+            subject.var = 'blubb'
+          end
+
+          it "renders properly" do
+            flash.should be_equal(flash_from_last_request)
+
+            subject.gaq_instructions.should \
+              be_eql([["_setAccount", nil], ["_setCustomVar", 0, :var, "blah", 3], ["_setCustomVar", 0, :var, "blubb", 3], ["_trackEvent", "last_cat", "last_action", "last_label"]])
+          end
+
+          context "with track_event" do
+            before(:each) do
+              subject.track_event 'category', 'action', 'label'
+            end
+
+            it 'renders properly' do
+              # flash.should be_empty
+              # FIXME documenting behaviour here that is probably wrong (double check if needed)
+              flash.should be_eql({:analytics_instructions=>[[["_setCustomVar", 0, :var, "blah", 3], ["_setCustomVar", 0, :var, "blubb", 3]], [["_trackEvent", "last_cat", "last_action", "last_label"], ["_trackEvent", "category", "action", "label"]]]})
+
+              subject.gaq_instructions.should \
+                be_eql([["_setAccount", nil], ["_setCustomVar", 0, :var, "blah", 3], ["_setCustomVar", 0, :var, "blubb", 3], ["_trackEvent", "last_cat", "last_action", "last_label"], ["_trackEvent", "category", "action", "label"]])
+            end
+          end
+
+          context "with next_request.track_event" do
+            before(:each) do
+              subject.next_request.track_event 'category', 'action', 'label'
+            end
+
+            it 'renders properly' do
+              flash.should be_eql({:analytics_instructions=>[[], [["_trackEvent", "category", "action", "label"]]]})
+
+              subject.gaq_instructions.should \
+                be_eql([["_setAccount", nil], ["_setCustomVar", 0, :var, "blah", 3], ["_setCustomVar", 0, :var, "blubb", 3], ["_trackEvent", "last_cat", "last_action", "last_label"]])
+            end
+          end
+        end
       end
     end
   end
