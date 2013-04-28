6e56614610f29932388d4bfc8d6c4866c4b19615
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-04-28 21:52:50 +0200

    make target characteristics a value object

--- lib/gaq/target_origin.rb
+++ lib/gaq/target_origin.rb
@@ -4,14 +4,18 @@ module Gaq
   class TargetOrigin
     attr_reader :default_target, :instruction_stack_pair
 
+    class Characteristics < Struct.new(:tracker_name, :is_for_next_request)
+      alias_method :for_next_request?, :is_for_next_request
+    end
+
     def initialize(instruction_stack_pair, next_request_promise)
       @instruction_stack_pair, @promise = instruction_stack_pair, next_request_promise
 
-      base_characteristict = characterize(nil, false)
-      @default_target = Target.new(self, base_characteristict, @instruction_stack_pair)
+      base_characteristics = characterize(nil, false)
+      @default_target = Target.new(self, base_characteristics, @instruction_stack_pair)
 
       @targets = {
-        base_characteristict => @default_target
+        base_characteristics => @default_target
       }
     end
 
@@ -29,26 +33,24 @@ module Gaq
     private
 
     def create_target(characteristics)
-      tracker_name = characteristics.first
-      is_for_next_request = characteristics[1]
-
-      if is_for_next_request
+      if characteristics.is_for_next_request
         instruction_stack_pair = (@next_request_instruction_stack_pair ||= @promise.call)
       else
         instruction_stack_pair = @instruction_stack_pair
       end
 
-      Target.new(self, characteristics, instruction_stack_pair, tracker_name)
+      Target.new(self, characteristics, instruction_stack_pair, characteristics.tracker_name)
     end
 
     def characterize_with_base(tracker_name, is_for_next_request, base)
-      tracker_name ||= base.first
-      is_for_next_request = base[1] if is_for_next_request.nil?
-      characterize(tracker_name, is_for_next_request)
+      result = base.dup
+      result.tracker_name = tracker_name if tracker_name
+      result.is_for_next_request = is_for_next_request if is_for_next_request
+      result
     end
 
     def characterize(tracker_name, is_for_next_request)
-      [tracker_name, is_for_next_request]
+      Characteristics.new(tracker_name, is_for_next_request)
     end
   end
 end
