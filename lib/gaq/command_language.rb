291d08639f4a521d84c82974860efb50ed698c3a
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-05-17 13:21:09 +0200

    CommandLanguage

---
+++ lib/gaq/command_language.rb
@@ -0,0 +1,85 @@
+# encoding: utf-8
+
+module Gaq
+  class CommandLanguage
+    attr_writer :value_coercer
+    attr_writer :value_preserializer
+    attr_writer :value_deserializer
+
+    def initialize
+      @descriptors = {}
+    end
+
+    CommandDescriptor = Struct.new(:signature, :name, :identifier)
+
+    def knows_command(identifier)
+      @descriptors[identifier] = CommandDescriptor.new.tap do |desc|
+        desc.identifier = identifier
+        yield desc
+      end
+      self
+    end
+
+    def commands_to_flash_items(commands)
+      commands.map do |command|
+        descriptor = @descriptors[command.identifier]
+
+        params = pre_serialize_params(command.params, descriptor.signature)
+
+        first_segment = [command.target_name, command.name].compact.join('.')
+        [first_segment, *params]
+      end
+    end
+
+    def commands_from_flash_items(flash_items)
+      flash_items.map do |flash_item|
+        descriptor, target_name = descriptor_and_target_name(flash_item.first)
+        params = deserialize_items(flash_item.drop(1), descriptor.signature)
+        Command.new(descriptor.identifier, descriptor.name, params, target_name)
+      end
+    end
+
+    def new_command(identifier, *params)
+      descriptor = @descriptors[identifier]
+      params = coerce_params(params, descriptor.signature)
+
+      Command.new(identifier, descriptor.name, params)
+    end
+
+    Command = Struct.new(:identifier, :name, :params, :target_name)
+
+    private
+
+    def descriptor_and_target_name(first_segment)
+      split = first_segment.split('.')
+      command_name, target_name = split.reverse
+      descriptor = @descriptors.values.find { |desc| desc.name == command_name }
+      [descriptor, target_name]
+    end
+
+    def coerce_params(params, signature)
+      signature = signature.take(params.length)
+      signature.zip(params).map do |type, param|
+        @value_coercer.call(type, param)
+      end
+    end
+
+    def pre_serialize_params(params, signature)
+      signature = signature.take(params.length)
+      signature.zip(params).map do |type, param|
+        @value_preserializer.call(type, param)
+      end
+    end
+
+    def deserialize_items(items, signature)
+      signature = signature.take(items.length)
+      signature.zip(items).map do |type, item|
+        @value_deserializer.call(type, item)
+      end
+    end
+
+    def self.singleton
+      # XXX
+    end
+  end
+end
---
+++ spec/lib/gaq/command_language_spec.rb
@@ -0,0 +1,165 @@
+require 'gaq/command_language'
+
+module Gaq
+  describe CommandLanguage do
+    let(:value_coercer) { double "value coercer" }
+    let(:value_preserializer) { double "value preserializer" }
+    let(:value_deserializer) { double "value deserializer" }
+
+    def null_coercing_and_serializing
+      value_coercer.stub(:call) { |type, value| value }
+      value_preserializer.stub(:call) { |type, value| value }
+    end
+
+    subject do
+      result = described_class.new
+      result.value_coercer = value_coercer
+      result.value_preserializer = value_preserializer
+      result.value_deserializer = value_deserializer
+      result
+    end
+
+    before do
+      subject.knows_command(:foo) do |desc|
+        desc.signature = [String]
+        desc.name = "_myFooCommand"
+      end if example.metadata[:foo_command]
+
+      subject.knows_command(:bar) do |desc|
+        desc.signature = [String, Integer]
+        desc.name = "_myBarCommand"
+      end if example.metadata[:bar_command]
+    end
+
+    describe ".new_command", foo_command: true do
+      it "returns a command with expected identifier, name and coerced params" do
+        value_coercer.should_receive(:call).with(String, "string").and_return "coerced string"
+
+        command = subject.new_command(:foo, "string")
+
+        command.identifier.should be == :foo
+        command.name.should be == "_myFooCommand"
+        command.params.should be == ["coerced string"]
+      end
+    end
+
+    describe ".commands_to_flash_items" do
+      let(:flash_data_item) do
+        flash_items = subject.commands_to_flash_items([command])
+        flash_items.should have(1).item
+        flash_items.first
+      end
+
+      context "with a command accepting two parameters", bar_command: true do
+        let(:command) do
+          command = CommandLanguage::Command.new
+          command.identifier = :bar
+          command.name = "_myBarCommand"
+          command.params = []
+          command
+        end
+
+        it "preserializes params into data items" do
+          value_preserializer.should_receive(:call).with(String, "first coerced string").and_return "first preserialized string"
+          value_preserializer.should_receive(:call).with(Integer, "second coerced string").and_return "second preserialized string"
+
+          command.params << "first coerced string" << "second coerced string"
+
+          flash_data_item.should have(3).items
+          flash_data_item[1].should be == "first preserialized string"
+          flash_data_item[2].should be == "second preserialized string"
+        end
+
+        it "preserializes only given params into data item even if less params given than signature length" do
+          value_preserializer.should_receive(:call).with(String, "first coerced string").and_return "first preserialized string"
+
+          command.params << "first coerced string"
+
+          flash_data_item.should have(2).items
+          flash_data_item[1].should be == "first preserialized string"
+        end
+      end
+
+      context "with a command accepting one parameter", foo_command: true do
+        let(:command) do
+          command = CommandLanguage::Command.new
+          command.identifier = :foo
+          command.name = "_myFooCommand"
+          command.params = ["coerced string"]
+          command
+        end
+
+        it "sets the first flash item's first item to the command name (no target set)" do
+          null_coercing_and_serializing
+
+          flash_data_item.should have(2).items
+          flash_data_item.first.should be == "_myFooCommand"
+        end
+
+        it "sets the first flash item's first item to the command name (target set)" do
+          null_coercing_and_serializing
+
+          command.target_name = "target"
+
+          flash_data_item.should have(2).items
+          flash_data_item.first.should be == "target._myFooCommand"
+        end
+      end
+    end
+
+    describe ".commands_from_flash_items", foo_command: true, bar_command: true do
+      it "correctly detects the command" do
+        flash_items = [
+          ["_myFooCommand"],
+          ["_myBarCommand"]
+        ]
+        commands = subject.commands_from_flash_items(flash_items)
+
+        commands.should have(2).items
+
+        command = commands.first
+        command.identifier.should be == :foo
+        command.name.should be == '_myFooCommand'
+
+        command = commands.last
+        command.identifier.should be == :bar
+        command.name.should be == '_myBarCommand'
+      end
+
+      it "deserializes parameters" do
+        params = ["first preserialized string", "second preserialized string"]
+
+        value_deserializer.should_receive(:call).with(String, params.first).and_return("first deserialized string")
+        value_deserializer.should_receive(:call).with(Integer, params.last).and_return("second deserialized string")
+
+        flash_item = ["_myBarCommand", *params]
+        commands = subject.commands_from_flash_items([flash_item])
+
+        commands.should have(1).item
+        command = commands.first
+
+        command.params.should be == ["first deserialized string", "second deserialized string"]
+      end
+
+      it "sets target name to nil when no explicit target given" do
+        flash_item = ["_myFooCommand"]
+        commands = subject.commands_from_flash_items([flash_item])
+
+        commands.should have(1).item
+        command = commands.first
+
+        command.target_name.should be_nil
+      end
+
+      it "correctly sets present target name" do
+        flash_item = ["my_target._myFooCommand"]
+        commands = subject.commands_from_flash_items([flash_item])
+
+        commands.should have(1).item
+        command = commands.first
+
+        command.target_name.should be == "my_target"
+      end
+    end
+  end
+end
