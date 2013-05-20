8a2bbf1c6c3bf0bb9296e47592d6e0d270c52cf8
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-05-20 14:57:00 +0200

    DslTarget

---
+++ lib/gaq/dsl_target.rb
@@ -0,0 +1,51 @@
+module Gaq
+  class DslTarget
+    def initialize(target_factory, factory_token, new_command_proc, commands, tracker_name)
+      @target_factory, @factory_token, @new_command_proc, @commands, @tracker_name =
+        target_factory, factory_token, new_command_proc, commands, tracker_name
+    end
+
+    def tracker(tracker_name)
+      @target_factory.target_with_tracker_name(tracker_name, @factory_token)
+    end
+
+    alias_method :[], :tracker
+
+    def next_request
+      @target_factory.target_for_next_request(@factory_token)
+    end
+
+    ## tracker methods
+
+    def track_event(category, action, label = nil, value = nil, noninteraction = nil)
+      array = [noninteraction, value, label, action, category]
+      4.times do
+        if array.first.nil?
+          array.shift
+        else
+          break
+        end
+      end
+
+      command = @new_command_proc.call(:track_event, *array.reverse)
+      command.tracker_name = @tracker_name
+      @commands << command
+    end
+
+    class << self
+      def tracker_commands_module(variables)
+        Module.new do
+          variables.each do |variable|
+            module_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+              def #{variable.name}=(value)
+                command = @new_command_proc.call(:set_custom_var, #{variable.slot}, #{variable.name.inspect}, value, #{variable.scope})
+                command.tracker_name = @tracker_name
+                @commands << command
+              end
+            RUBY_EVAL
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/gaq/dsl_target_spec.rb
@@ -0,0 +1,87 @@
+require 'gaq/dsl_target'
+
+require 'gaq/configuration'
+require 'gaq/command_language'
+
+module Gaq
+  describe DslTarget do
+    let(:target_factory) { double "target factory" }
+    let(:factory_token) { double "factory token" }
+    let(:new_command_proc) { double "new_command_proc" }
+    let(:commands) { double "commands" }
+    let(:tracker_name) { "custom tracker name" }
+
+    shared_context with_instance_subject: true do
+      subject do
+        described_class.new(target_factory, factory_token, new_command_proc, commands, tracker_name)
+      end
+    end
+
+    def command_expecting_tracker_name
+      result = double
+      result.should_receive(:tracker_name=).with(tracker_name)
+      commands.should_receive(:<<).with(result).once
+      result
+    end
+
+    describe "#tracker", with_instance_subject: true do
+      it "delegates to target factory, passing token" do
+        tracker_name = "tracker name"
+
+        target_factory.should_receive(:target_with_tracker_name)
+          .with(tracker_name, factory_token)
+          .and_return(5)
+
+        subject.tracker(tracker_name).should be 5
+      end
+    end
+
+    describe "#next_request", with_instance_subject: true do
+      it "delegates to target factory, passing token" do
+        target_factory.should_receive(:target_for_next_request)
+          .with(factory_token)
+          .and_return(5)
+
+        subject.next_request.should be 5
+      end
+    end
+
+    describe "#track_event", with_instance_subject: true do
+      it "foo" do
+        command = command_expecting_tracker_name
+
+        new_command_proc.should_receive(:call)
+          .with(:track_event, "3", "5")
+          .and_return(command)
+
+        subject.track_event("3", "5")
+      end
+    end
+
+    describe ".tracker_commands_module" do
+      let(:variable) do
+        result = Configuration::Variable.new(4, "myvar", 2)
+        result.slot.should be == 4
+        result.name.should be == "myvar"
+        result.scope.should be == 2
+        result
+      end
+
+      subject do
+        cls = Class.new(described_class)
+        cls.send :include, described_class.tracker_commands_module([variable])
+        cls.new(target_factory, factory_token, new_command_proc, commands, tracker_name)
+      end
+
+      it "creates a variable setter that pushes a set_custom_var command" do
+        command = command_expecting_tracker_name
+
+        new_command_proc.should_receive(:call)
+          .with(:set_custom_var, 4, "myvar", "value", 2)
+          .and_return(command)
+
+        subject.myvar = "value"
+      end
+    end
+  end
+end
